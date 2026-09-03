aa28c21568e6c5f22c040a623aff7fa4ed28dcaa
torexto <tomasztrela8@gmail.com>
2026-09-03 22:13:06 +0200

    api concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import {
+   QueryClient,
+   QueryClientProvider,
+   useQuery,
+} from "@tanstack/react-query";
+import { getEnergyPrices } from "./lib/pse";
+
+const queryClient = new QueryClient();
+
 function App() {
+   return (
+      <QueryClientProvider client={queryClient}>
+         <Test />
+      </QueryClientProvider>
+   );
+}
+
+function Test() {
+   const { data, isLoading, error } = useQuery({
+      queryFn: getEnergyPrices,
+      queryKey: ["energy-prices"],
+   });
+
    return <></>;
 }
 
